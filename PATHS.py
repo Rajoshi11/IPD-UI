64c5e19e610f7419185a55160b7d1e957c462d04
Rajoshi11 <ruju111202@gmail.com>
2023-02-26 16:58:45 +0530

    navbar added

---
+++ PATHS.py
@@ -0,0 +1,12 @@
+NAVBAR_PATHS = {
+    'HOME':'home',
+    'ML DATASET EXPLORER': 'MLDatasetExplorer',
+    'AUTO ML REGRESSION APP': 'AutoMlRegressionApp',
+    'AUTO ML CLASSIFICATION APP': 'AutoMlClassificationApp',
+    'THE MACHINE LEARNING HYPERPARAMETER OPTIMIZATION APP': 'TheMachineLearningHyperparameterOptimizationApp'
+}
+
+SETTINGS = {
+    'OPTIONS':'options',
+    'CONFIGURATION':'configuration'
+}
